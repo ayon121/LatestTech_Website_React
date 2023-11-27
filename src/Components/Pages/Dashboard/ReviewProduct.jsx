155ff7f11af9fa0aeb9810a21fa31d2166451cc7
ayon121 <borshonsaha1234@gmail.com>
2023-11-28 00:33:53 +0600

    approve & reject function done

--- src/Components/Pages/Dashboard/ReviewProduct.jsx
+++ src/Components/Pages/Dashboard/ReviewProduct.jsx
@@ -5,7 +5,9 @@ import { axiosSecure } from '../../Hooks/useAxiosSecure';
 // react toast
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css'
+import useReviewProducts from '../../Hooks/useReviewProducts';
 const ReviewProduct = ({ product }) => {
+    const [ , refetch] = useReviewProducts()
     const { _id, status, product_name, product_img, tags, description, user_email, user_img, total_upvote, external_link, user_name } = product
 
 
@@ -23,16 +25,27 @@ const ReviewProduct = ({ product }) => {
 
         }
 
-        axiosSecure.post('/allproducts' , approvedProduct)
-        .then(res => {
-            if(res.data.insertedId){
-                toast('Product Approved Successfuly')
-                axiosSecure.patch(`/reviewproduct/${_id}`)
-                .then(res => {
-                    console.log(res.data);
-                })
-            }
-        })
+        axiosSecure.post('/allproducts', approvedProduct)
+            .then(res => {
+                if (res.data.insertedId) {
+                    toast('Product Approved Successfuly')
+                    axiosSecure.patch(`/reviewproduct/${_id}`)
+                        .then(res => {
+                            console.log(res.data);
+                            refetch()
+                        })
+                }
+            })
+
+
+    }
+
+    const RejectProduct = () => {
+        axiosSecure.patch(`/reviewproduct/reject/${_id}`)
+            .then(res => {
+                console.log(res.data);
+                refetch()
+            })
     }
     return (
         <div className="card card-compact w-auto h-96 bg-base-100 shadow-xl font-inter">
@@ -46,8 +59,8 @@ const ReviewProduct = ({ product }) => {
                 <p>{user_email}</p>
                 <div className="card-actions justify-start items-center pt-2 pb-2 ">
                     <div className='flex items-center gap-2 px-2 py-1 rounded-xl'>
-                        <button onClick={handleAproveProduct} className="btn">Approve</button>
-                        <button className="btn">Reject</button>
+                        <button onClick={handleAproveProduct} className={`btn`} disabled={status === 'Approved'}>Approve</button>
+                        <button onClick={RejectProduct} disabled={status === 'Rejected'} className="btn">Reject</button>
                     </div>
                 </div>
             </div>
