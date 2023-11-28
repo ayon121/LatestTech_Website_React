dc066ac196ba8a9aed905f30c1725cff53e53fa7
ayon121 <borshonsaha1234@gmail.com>
2023-11-29 00:30:48 +0600

    make featured  function done

--- src/Components/Pages/AllProducts/AllProduct.jsx
+++ src/Components/Pages/AllProducts/AllProduct.jsx
@@ -1,10 +1,29 @@
 
 import PropTypes from 'prop-types';
 import { BiSolidDownvote, BiSolidUpvote } from 'react-icons/bi';
+import useSingleUser from '../../Hooks/useSingleUser';
+import { axiosSecure } from '../../Hooks/useAxiosSecure';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
 
 const AllProduct = ({allproduct}) => {
-    const {product_img , product_name , total_upvote , tags  } = allproduct
-    
+    const {product_img , product_name , total_upvote , tags ,user_email , post_date } = allproduct
+    const [singleuserInfo] = useSingleUser()
+    const handlemakeFeatured = () => {
+        
+        const addfeatured = {
+            product_img , product_name , total_upvote , tags , user_email, post_date
+        }
+
+        axiosSecure.post('/featured' , addfeatured)
+        .then(res => {
+            console.log(res.data);
+            if(res.data.insertedId){
+                toast(`successfully added to Featured`)
+            }
+            
+        })
+    }
     return (
         <div className="card card-compact w-auto h-96 bg-base-100 shadow-xl font-inter">
             <figure><img src={product_img} alt="featured img" /></figure>
@@ -23,7 +42,17 @@ const AllProduct = ({allproduct}) => {
                         <p>{total_upvote}upvotes</p>
                     </div>
                 </div>
+               <hr />
+                <div>
+                    {
+                        singleuserInfo[0]?.userRole === 'modaretor' &&  <button onClick={handlemakeFeatured} className="btn btn-sm border-[#FF444A]">Make Featured</button>
+                    }
+                    {
+                        singleuserInfo[0]?.userRole === 'admin' &&  <button onClick={handlemakeFeatured} className="btn btn-sm border-[#FF444A]">Make Featured</button>
+                    }
+                </div>
             </div>
+            <ToastContainer />
         </div>
     );
 };
